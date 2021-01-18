8bb89fa4a8cea09545ee8a0ce3afd20690f1596c
marry705 <marry705chilo@gmail.com>
2021-01-18 20:07:42 +0300

    doc: clean packege file

--- src/components/ButtonsPanel/UndoButtonsPanel.test.tsx
+++ src/components/ButtonsPanel/UndoButtonsPanel.test.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, cleanup, screen, fireEvent } from '@testing-library/react';
+import {
+  render, cleanup, screen, fireEvent,
+} from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 
 import { createStore, Store } from 'redux';
